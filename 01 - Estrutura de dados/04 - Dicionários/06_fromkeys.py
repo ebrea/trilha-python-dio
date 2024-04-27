88665826892b0374e720372a0bd4ee41f917b99f
Guilherme Carvalho <gui.ifsp11@gmail.com>
2024-04-27 15:31:49 -0300

    feat: add module 13 files

--- 01 - Estrutura de dados/04 - Dicionários/06_fromkeys.py
+++ 01 - Estrutura de dados/04 - Dicionários/06_fromkeys.py
@@ -1,5 +1,8 @@
-resultado = dict.fromkeys(["nome", "telefone"])  # {"nome": None, "telefone": None}
-print(resultado)
 
-resultado = dict.fromkeys(["nome", "telefone"], "vazio")  # {"nome": "vazio", "telefone": "vazio"}
-print(resultado)
+#  Cria chaves sem valor (valor nulo: none) ou com um valor padrão
+
+d1 = dict.fromkeys(["nome", "telefone"])  # {"nome": None, "telefone": None}
+print(d1)
+
+d2 = dict.fromkeys(["nome", "telefone"], "cep")  # {"nome": "vazio", "telefone": "cep"}
+print(d2)
