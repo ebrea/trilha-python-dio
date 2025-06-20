453539e8136b013bf30485b524f3202acedccd41
ebrea <eberbrea@gmail.com>
2025-06-20 14:51:18 -0300

    feat: add module 13 files

--- 01 - Estrutura de dados/04 - Dicionários/14_values.py
+++ 01 - Estrutura de dados/04 - Dicionários/14_values.py
@@ -7,5 +7,15 @@ contatos = {
 
 resultado = (
     contatos.values()
-)  # dict_values([{'nome': 'Guilherme', 'telefone': '3333-2221'}, {'nome': 'Giovanna', 'telefone': '3443-2121'}, {'nome': 'Chappie', 'telefone': '3344-9871'}, {'nome': 'Melaine', 'telefone': '3333-7766'}])  # noqa
+)
 print(resultado)
+
+'''
+Resultado:
+dict_values([
+    {'nome': 'Guilherme', 'telefone': '3333-2221'}, {'nome': 'Giovanna', 'telefone': '3443-2121'}, 
+    {'nome': 'Chappie', 'telefone': '3344-9871'}, {'nome': 'Melaine', 'telefone': '3333-7766'}
+])
+
+'''
+
